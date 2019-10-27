33dd174d99fd0431e9fca410b57491c2f7d0d7d4
Alex shevelo <oshevelo@softserveinc.com>
2019-10-27 10:54:31 +0200

    demo first model

--- FirstShop/apps/shipments/models.py
+++ FirstShop/apps/shipments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+#from apps.orders.models import Order
 
-# Create your models here.
+
+class Shipment(models.Model):
+    pub_order_id = models.CharField()
+    #FIXME: enable foreign key after order impl
+    #order = models.ForeignKey(
+    #    'Order',
+    #    on_delete=models.CASCADE,
+    #)
+    
