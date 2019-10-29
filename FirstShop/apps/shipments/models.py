6d278b2b5c403547e06fa51c8a6ff10a128cbe9b
yarique <yariuezz@gmail.com>
2019-10-29 16:14:30 +0200

    change delivery_status field to choice

--- FirstShop/apps/shipments/models.py
+++ FirstShop/apps/shipments/models.py
@@ -5,6 +5,10 @@ from django.utils import timezone
 # from apps.orders.models import Payment
 
 # from apps.orders.models import Order
+city = 'Kyiv'
+branch = 'Branch #1'
+DateToReceive = '01-12-2019 12:00:00'
+DateReceived = '02-12-2019 12:00:00'
 
 
 class Shipment(models.Model):
@@ -49,6 +53,7 @@ class Shipment(models.Model):
         (CardToCard, 'Card to Card payment'),
         (Certificate, 'Certificate payment')
     ]
+
     payment_method = models.CharField(
         max_length=3,
         choices=PAYMENT_METHOD,
@@ -58,7 +63,56 @@ class Shipment(models.Model):
     delivery_address = models.CharField(max_length=200)
 
     # Shipment status field
-    shipment_status = models.IntegerField(default=1)
+
+    WaitForSender = 1
+    DeletedForm = 2
+    NumberNotFound = 3
+    DeliveryInOblast = 4
+    DeliveryInLocal = 41
+    DeliveryOnTheWay = 5
+    DeliveryInTheCity = 6
+    DeliveryOnTheBranch_1 = 7
+    DeliveryOnTheBranch_2 = 8
+    DeliveryReceived = 9
+    DeliveryReceivedCashback = 10
+    DeliveryReceivedCashbackGet = 11
+    DeliveryReceivedCheck = 14
+    OnTheWayToCustomer = 101
+    RejectBySender_1 = 102
+    RejectBySender_2 = 103
+    RejectBySender_3 = 108
+    DeliveryAddressChange = 104
+    DeliveryHoldEnd = 105
+    DeliveryBack = 106
+
+    DELIVERY_STATUS = [
+        (WaitForSender, 'Нова пошта очікує надходження від відправника'),
+        (DeletedForm, 'Видалено'),
+        (NumberNotFound, 'Номер не знайдено'),
+        (DeliveryInOblast, f'Відправлення у місті {city}'),
+        (DeliveryInLocal, f'Відправлення у місті {city}'),
+        (DeliveryOnTheWay, 'Відправлення прямує до міста'),
+        (DeliveryInTheCity, f'Відправлення у місті {city}, орієнтовна доставка до ВІДДІЛЕННЯ-{branch} {DateToReceive} Очікуйте додаткове повідомлення про прибуття.'),
+        (DeliveryOnTheBranch_1, 'Прибув на відділення'),
+        (DeliveryOnTheBranch_2, 'Прибув на відділення'),
+        (DeliveryReceived, 'Відправлення отримано'),
+        (DeliveryReceivedCashback, f'Відправлення отримано {DateReceived}. Протягом доби ви одержите SMS-повідомлення про надходження грошового переказу та зможете отримати його в касі відділення «Нова пошта»'),
+        (DeliveryReceivedCashbackGet, f'Відправлення отримано {DateReceived}. Грошовий переказ видано одержувачу.'),
+        (DeliveryReceivedCheck, 'Відправлення передано до огляду отримувачу'),
+        (OnTheWayToCustomer, 'На шляху до одержувача'),
+        (RejectBySender_1, 'Відмова одержувача'),
+        (RejectBySender_2, 'Відмова одержувача'),
+        (RejectBySender_3, 'Відмова одержувача'),
+        (DeliveryAddressChange, 'Змінено адресу'),
+        (DeliveryHoldEnd, 'Припинено зберігання'),
+        (DeliveryBack, 'Одержано і створено ЄН зворотньої доставки')
+    ]
+
+    shipment_status = models.IntegerField(
+        default=WaitForSender,
+        choices=DELIVERY_STATUS
+    )
+
     shipment_status_date = models.DateTimeField('status_date', default=timezone.now)
 
     def __str__(self):
