a03b5a4b4e8f5eb6acf356fe102896765e339812
I.Galytska <iryna.galytska@sanoma.com>
2019-11-05 22:02:20 +0200

    add model draft

--- FirstShop/apps/catalog/admin.py
+++ FirstShop/apps/catalog/admin.py
@@ -1,5 +1,8 @@
-from apps.catalog.models import Category
-
 from django.contrib import admin
 
-admin.site.register(Category)
+from apps.catalog.models import Genre, Book, Author, Language
+
+admin.site.register(Genre)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Language)
--- FirstShop/apps/catalog/models.py
+++ FirstShop/apps/catalog/models.py
@@ -1,8 +1,51 @@
 from django.db import models
 
 
-class Category(models.Model):
-    title = models.CharField(max_length=50)
+class Genre(models.Model):
+    """Model representing a book genre."""
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
 
     def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+
+class Author(models.Model):
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.last_name}, {self.first_name}'
+
+
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
+
+
+class Book(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    title = models.CharField(max_length=200)
+
+    # Foreign Key used because book can only have one author, but authors can have multiple books
+    # Author as a string rather than object because it hasn't been declared yet in the file
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+
+    def __str__(self):
+        """String for representing the Model object."""
         return self.title
+
+    # language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
+    language = models.ManyToManyField(Language, help_text='Select language/languages for this book')
