1cc962594a167f231ae6f7dbdb8fcb48b3a8faec
Viktor Vovchak <viktorvovchak@MBP-Viktor.WORKGROUP>
2019-11-07 20:26:59 +0200

    models

--- FirstShop/apps/orders/admin.py
+++ FirstShop/apps/orders/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'public_id',
+        'user',
+        'delivery',
+        'payment',
+        'status',
+        'date'
+    )
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'order',
+        #'product_id',
+        'amount',
+        'price',
+        'discount',
+
+    )
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
