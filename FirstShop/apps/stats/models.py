024d238b8a414bd05a7c52df43de5155fb0862b4
PolinaVoronova1 <56888702+PolinaVoronova1@users.noreply.github.com>
2019-11-10 00:50:41 +0200

    Update models.py

--- FirstShop/apps/stats/models.py
+++ FirstShop/apps/stats/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+import datetime
+from django.utils import timezone
+
+
+from django.db import models
+
+
+class Product(models.Model):
+	class Meta:
+		db_table = "product"
+	
+	product_name = models.CharField(max_length = 200)
+	
+	def __str__(self):
+		return self.product_name
+
+
+class ProductStatistics(models.Model):
+	class Meta:
+    db_table = "productStats"
+     
+	product = models.ForeignKey(Product, on_delete = models.PROTECT)                  
+	date = models.DateField('Date', default=timezone.now)
+	views = models.IntegerField('Views', default=0)
+     
+	def __str__(self):
+		return self.product.product_name
+     
