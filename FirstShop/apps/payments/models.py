185a70fa10c53a409b6ebd3e40b753f9a220d1b6
JhinDanzo <gentj03@icloud.com>
2019-11-15 20:57:43 +0200

    payment model fix

--- FirstShop/apps/payments/models.py
+++ FirstShop/apps/payments/models.py
@@ -4,12 +4,18 @@ from django.utils import timezone
 
 # Create your models here.
 
+PAY_PROVIDER = [
+    ['Visa', 'Visa'],
+    ['MasterCard', 'MasterCard']
+]
+
 class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=False)
     payment_id = models.CharField(max_length=100,null=True, blank=False)
     #product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=False)
     amount = models.DecimalField(default=0, max_digits=10, decimal_places=6, null=True, blank=False)
     date = models.DateTimeField(default=timezone.now, null=True, blank=False)
+    provider = models.CharField(max_length=100, choices=PAY_PROVIDER, null=True, blank=False)
 
     def __str__(self):
         return 'user = {}, payment_id = {}'.format(self.user, self.payment_id, self.pk)
