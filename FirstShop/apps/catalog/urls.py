0ec12b645036357075e9851163dda8616f394826
I.Galytska <iryna.galytska@sanoma.com>
2019-11-16 17:36:06 +0200

    ad sub categories serializer

--- FirstShop/apps/catalog/urls.py
+++ FirstShop/apps/catalog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from apps.catalog import views
 
 urlpatterns = [
-    path('all-categories-list/', views.CategoryList.as_view(), name='all_categories_list')
+    path('categories/', views.CategoryList.as_view(), name='all_categories_list'),
+    path('categories/<int:pk>/', views.CategoryDetail.as_view())
 ]
