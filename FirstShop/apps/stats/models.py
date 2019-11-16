ec7f771a2e209155bd3d44e6fcfb589dd1a1eca0
PolinaVoronova1 <56888702+PolinaVoronova1@users.noreply.github.com>
2019-11-17 00:19:35 +0200

    Update models.py

--- FirstShop/apps/stats/models.py
+++ FirstShop/apps/stats/models.py
@@ -1,7 +1,7 @@
+from datetime import datetime, timedelta
 from django.db import models
-
-import datetime
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Product(models.Model):
@@ -16,7 +16,7 @@ class Product(models.Model):
 
 class ProductStatistics(models.Model):
 	class Meta:
-    		db_table = "productStats"
+        	db_table = "statistics"
      
 	product = models.ForeignKey(Product, on_delete = models.PROTECT)                  
 	date = models.DateField('Date', default=timezone.now)
@@ -24,4 +24,17 @@ class ProductStatistics(models.Model):
      
 	def __str__(self):
 		return self.product.product_name
+
+class UserStatistics(models.Model):
+	class Meta:
+        	db_table = "user_statistics"
      
+	user = models.ForeignKey(User, on_delete = models.PROTECT)                  
+	date = models.DateField('Date', default=timezone.now)
+	session = models.DurationField('Session', default=0)
+	source = models.CharField(max_length = 200)
+     
+	def __str__(self):
+		return self.user
+     
+
