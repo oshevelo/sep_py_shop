0e29b6e1895cacb8b1840b96078842891219a59a
PolinaVoronova1 <56888702+PolinaVoronova1@users.noreply.github.com>
2019-11-17 00:17:09 +0200

    Update admin.py

--- FirstShop/apps/stats/admin.py
+++ FirstShop/apps/stats/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 
 from .models import Product
 from .models import ProductStatistics
+from .models import UserStatistics
 
 
 admin.site.register(Product)
 admin.site.register(ProductStatistics)
+admin.site.register(UserStatistics)
+
 
