ef4fd43acf0e16aa3f23882e27cd3959d28c4145
FibZIII <soloninandr@gmail.com>
2019-11-17 10:38:27 +0200

    product_v0.3

--- FirstShop/apps/products/models.py
+++ FirstShop/apps/products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length = 200)
+    product_price = models.FloatField(default = 0)
+    product_avaliable_count = models.PositiveIntegerField(default = 0, null = True)
+    product_detail = models.TextField(default = "", editable = True)
+    product_can_be_sold = models.BooleanField(default = True)
+    product_created_updated = models.DateTimeField(auto_now_add = True)
+    #author_name= models.ForeignKey(Author, on_delete=models.SET_NULL, null = True)
+    #gengre = models.ManyToManyField(Gengre)
+    #publishing_house = models.ForeignKey(Publishing_house, on_delete=models.SET_NULL, null = True) 
+    Publication_date = models.DateTimeField()
+    Number_of_pages = models.IntegerField(default = 0)
+    def __str__(self):
+        return self.product_name
