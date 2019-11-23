9e3f3c345da165a35963f514ffffb54315a80217
JhinDanzo <51052074+JhinDanzo@users.noreply.github.com>
2019-11-23 12:21:36 +0200

    Update settings.py

--- FirstShop/FirstShop/settings.py
+++ FirstShop/FirstShop/settings.py
@@ -138,8 +138,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 from FirstShop.local import *
