d897b055a282516e6e72d6f7a2a27256844fb22d
Iryna Galytska <iryna.galytska@materialise.kiev.ua>
2019-11-26 16:01:59 +0200

    add create example

--- FirstShop/apps/catalog/serializers.py
+++ FirstShop/apps/catalog/serializers.py
@@ -9,11 +9,10 @@ class TopCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
 
-        fields = ('id', 'category_name')
+        fields = ('id',)
 
 
 class CategorySerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Category
 
@@ -21,16 +20,25 @@ class CategorySerializer(serializers.ModelSerializer):
 
     def get_fields(self):
         fields = super(CategorySerializer, self).get_fields()
-        fields['sub_category'] = CategorySerializer(many=True)
-        fields['top_category'] = TopCategorySerializer()
+        fields['sub_category'] = CategorySerializer(many=True, allow_null=True)
+        fields['top_category'] = TopCategorySerializer(required=False, allow_null=True)
 
         return fields
 
     def create(self, validated_data):
+        # ? Keep remove sub cat data ?
         sub_categories_data = validated_data.pop('sub_category')
+        top_category = validated_data.pop('top_category')
         category = Category.objects.create(**validated_data)
-        for sub_category_data in sub_categories_data:
-            # How to solve error Error in formatting: RecursionError: maximum recursion depth exceeded
-            # create() got multiple values for keyword argument 'top_category'
-            Category.objects.create(top_category=category, **sub_category_data)
+        # Is this equivalent to
+        # category = super().create(**validated_data)
+        if top_category:
+            # TBD ad id verification
+            top_cat = Category.objects.get(top_category['id'])
+            category.top_category = top_cat
+            category.save()
+        # for sub_category_data in sub_categories_data:
+        # How to solve error Error in formatting: RecursionError: maximum recursion depth exceeded
+        # create() got multiple values for keyword argument 'top_category'
+        # Category.objects.create(top_category=category, **sub_category_data)
         return category
