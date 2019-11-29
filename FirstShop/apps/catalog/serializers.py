01576c0ff2ef8fa37d399f37077601a52be53758
Iryna Galytska <iryna.galytska@materialise.kiev.ua>
2019-11-29 12:01:33 +0200

    add test cases draft

--- FirstShop/apps/catalog/serializers.py
+++ FirstShop/apps/catalog/serializers.py
@@ -4,41 +4,45 @@ from apps.catalog.models import Category
 
 
 class TopCategorySerializer(serializers.ModelSerializer):
-    # TBD How to add nested top categories ?
-
     class Meta:
         model = Category
 
-        fields = ('id',)
+        fields = ['id']
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
 
-        fields = ('id', 'category_name', 'top_category', 'sub_category')
+        fields = ['id', 'category_name', 'top_category', 'sub_category']
 
     def get_fields(self):
         fields = super(CategorySerializer, self).get_fields()
         fields['sub_category'] = CategorySerializer(many=True, allow_null=True)
         fields['top_category'] = TopCategorySerializer(required=False, allow_null=True)
+        
 
         return fields
 
     def create(self, validated_data):
+        # ? Created with 2 post requests 1 top 2 sub
+
         # ? Keep remove sub cat data ?
+        print('22222222222222222222222---------------------------------------------------------')
+        for k, v in validated_data:
+            print(k, v)
+        print(len(validated_data['top_category']))
         sub_categories_data = validated_data.pop('sub_category')
         top_category = validated_data.pop('top_category')
         category = Category.objects.create(**validated_data)
         # Is this equivalent to
         # category = super().create(**validated_data)
+
         if top_category:
+            print('---------------------------------------------------------')
+            print(top_category)
             # TBD ad id verification
             top_cat = Category.objects.get(top_category['id'])
             category.top_category = top_cat
             category.save()
-        # for sub_category_data in sub_categories_data:
-        # How to solve error Error in formatting: RecursionError: maximum recursion depth exceeded
-        # create() got multiple values for keyword argument 'top_category'
-        # Category.objects.create(top_category=category, **sub_category_data)
         return category
--- FirstShop/apps/catalog/tests.py
+++ FirstShop/apps/catalog/tests.py
@@ -1,12 +1,52 @@
+import json
+
 from django.test import TestCase
+from django.test import tag
+from apps.catalog.models import Category
 
 
 class SimpleTest(TestCase):
 
-    def test_details(self):
-        pass
-        # Issue a GET request.
-        response = self.client.get('/catalog/categories/')
+    def setUp(self):
+        top_category = Category.objects.create(category_name='test_top_category')
+        Category.objects.create(category_name='test_sub_category', top_category=top_category)
 
-        # Check that the response is 200 OK.
-        self.assertEqual(response.status_code, 200)
+    # @tag('smoke')
+    # def test_get_list_smoke(self):
+    #     response = self.client.get('/catalog/categories/')
+    #
+    #     self.assertContains(response, "test_top_category", status_code=200)
+    #
+    # def test_get_category_details(self):
+    #     expected_category_detail = {'id': 1, 'category_name': 'test_top_category', 'top_category': None,
+    #                                 'sub_category': [
+    #                                     {'id': 2, 'category_name': 'test_sub_category', 'top_category': {'id': 1},
+    #                                      'sub_category': []}]}
+    #
+    #     response = self.client.get('/catalog/categories/1/')
+    #
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(response.json(), expected_category_detail)
+
+    def test_post_categories(self):
+        test_json = json.dumps({
+            "category_name": "parent",
+            "top_category": None,
+            "sub_category": []})
+
+        # TBD get top cat id
+        test_json_child = {
+            "category_name": "child",
+            "top_category": {"id": 3},
+            "sub_category": []
+        }
+        response = self.client.post('/catalog/categories/', data=test_json, content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        response = self.client.post('/catalog/categories/', data=test_json_child, content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        response = self.client.get('/catalog/categories/4/')
+        response_dict = response.json()
+        actual_top_cat = response.json()['top_category']
+        print('----------------------', actual_top_cat)
+        self.assertTrue(actual_top_cat)
+
