ef67422409604067fe9f0a8a3814963455882b6c
Alex shevelo <oshevelo@softserveinc.com>
2019-12-01 11:49:46 +0200

    search init

--- FirstShop/apps/products/views.py
+++ FirstShop/apps/products/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import ProductSerializer
 from .models import Product 
-
+from .filters import ProductFilter
 
 
 class ProductView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_class = ProductFilter
+
+    def get_queryset(self, *args, **kwargs):
+        if self.request.GET.get('search'):
+            return Product.objects.filter(product_name__icontains=self.request.GET.get('search'))
+        return Product.objects.all()
+
+
 class SingleProductView(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
