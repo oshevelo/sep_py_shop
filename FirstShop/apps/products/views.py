715a7c3a9d285e2f46d64248cfb7bb6fcb1781a3
FibZIII <soloninandr@gmail.com>
2019-12-01 11:09:16 +0200

    complect

--- FirstShop/apps/products/views.py
+++ FirstShop/apps/products/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .serializers import ProductSerializer
-from .models import Product 
+from .serializers import ProductSerializer ComplectSerializer
+from .models import Product, Complect
 
 
 
 class ProductView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
 class SingleProductView(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class ComplectView(ListCreateAPIView):
+    queryset = Complect.objects.all()
+    serializer_class = ComplectSerializer
+
+class SingleComplectView(RetrieveUpdateDestroyAPIView):
+    queryset = Complect.objects.all()
+    serializer_class = ComplectSerializer
