a3828b70b32cb69e520c9e6e84457e444ce4e177
JhinDanzo <gentj03@icloud.com>
2019-12-05 13:48:20 +0200

    changing model

--- FirstShop/apps/payments/serializers.py
+++ FirstShop/apps/payments/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from .models import Payment
+from .models import Payment, BillingLogs
 
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
-        fields = ['user', 'payment_id', 'amount', 'date', 'provider']
+        fields = ['id', 'user', 'public_id', 'product', 'amount', 'date', 'provider']
