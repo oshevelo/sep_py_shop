861a42b8e1673e427813b4e56541459ba39474d9
Viktor Vovchak <viktorvovchak@MacBook-Pro-Viktor.local>
2019-12-07 17:35:27 +0200

    add search and try to do serializers

---
+++ FirstShop/apps/products/filters.py
@@ -0,0 +1,19 @@
+import django_filters
+from django.db.models import Q
+from django_filters.rest_framework import FilterSet
+from .models import Product
+
+
+class ProductFilter(FilterSet):
+
+    def product_name_contains(self, qs, contains, value):
+        return qs.filter(name__icontains=value)
+
+
+    product_name = django_filters.filters.CharFilter(method='product_name_contains')
+
+
+    class Meta:
+        model = Product
+        fields = ['id']
+
