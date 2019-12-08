da1134405c605465b17d7416e9fac2eed010de81
FibZIII <soloninandr@gmail.com>
2019-12-08 11:38:56 +0200

    complects

--- FirstShop/apps/products/models.py
+++ FirstShop/apps/products/models.py
@@ -2,16 +2,27 @@ from django.db import models
 
 
 class Product(models.Model):
-    product_name = models.CharField(max_length = 200)
-    product_price = models.FloatField(default = 0)
-    product_avaliable_count = models.PositiveIntegerField(default = 0, null = True)
-    product_detail = models.TextField(default = "", editable = True)
-    product_can_be_sold = models.BooleanField(default = True)
-    product_created_updated = models.DateTimeField(auto_now_add = True)
-    #author_name= models.ForeignKey(Author, on_delete=models.SET_NULL, null = True)
-    #gengre = models.ManyToManyField(Gengre)
+    name = models.CharField(max_length = 200)
+    price = models.FloatField(default = 0)
+    avaliable_count = models.PositiveIntegerField(default = 0, null = True)
+    detail = models.TextField(default = "", editable = True)
+    active = models.BooleanField(default = True)
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
+    #author= models.ForeignKey('Author', on_delete=models.SET_NULL, null = True)
+    #gengre = models.ManyToManyField('Gengre')
     #publishing_house = models.ForeignKey(Publishing_house, on_delete=models.SET_NULL, null = True) 
-    Publication_date = models.DateTimeField()
-    Number_of_pages = models.IntegerField(default = 0)
+    publication_date = models.DateTimeField()
+    number_of_pages = models.IntegerField(default = 0)
+    
+    
+    def  __str__(self):
+        return self.name
+class Complect(models.Model): 
+    name = models.CharField(max_length = 200)
+    detail = models.TextField(default = "", editable = True)
+    discount = models.DecimalField(default = 0, max_digits=2, decimal_places = 2, null=True, blank=True)
+    products = models.ManyToManyField(Product)
     def __str__(self):
-        return self.product_name
+        return self.name
+        
