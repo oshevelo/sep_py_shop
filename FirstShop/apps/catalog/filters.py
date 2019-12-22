ac3d30c07b071262041758fb5a225967dbeb5f73
I.Galytska <iryna.galytska@sanoma.com>
2019-12-22 17:47:28 +0200

    redo product by category filter

--- FirstShop/apps/catalog/filters.py
+++ FirstShop/apps/catalog/filters.py
@@ -1,17 +1,15 @@
 import django_filters
-
 from django_filters.rest_framework import FilterSet
 
 
-class ProductByCategoryIdFilter(FilterSet):
+class CatalogProductFilter(FilterSet):
     # filtering url e.g. /catalog/categories/products/?category_ids=1,2
-    # /catalog/categories/products/?products_name=Book title&category_ids=1
+    # /catalog/categories/products/?product_name=Book title&category_ids=1
     class NumberInFilter(django_filters.BaseInFilter, django_filters.NumberFilter):
         pass
 
-    category_ids = NumberInFilter(field_name='id', lookup_expr='in')
+    category_ids = NumberInFilter(field_name='categories__id', lookup_expr='in', distinct=True)
 
-    # e.g./catalog/categories/products/?products_name=Book title
-    # ? How to NOT include not matching products ?
-    products_name = django_filters.CharFilter(
-        field_name='products__name', lookup_expr='iexact')
+    # e.g./catalog/categories/products/?product_name=Book title
+    product_name = django_filters.CharFilter(
+        field_name='name', lookup_expr='iexact')
